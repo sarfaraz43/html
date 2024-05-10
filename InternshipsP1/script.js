59205904ea50e9831862a14346f2a2d5e44037fd
sarfaraz43 <151362022+sarfaraz43@users.noreply.github.com>
2024-05-10 17:11:17 +0530

    Delete InternshipsP1 directory

--- InternshipsP1/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll',function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 100) {
-        navbar.style.backgroundColor = '400';
-    } 
-    else {
-        navbar.style.backgroundColorackgr = '600';
-    }
-});
