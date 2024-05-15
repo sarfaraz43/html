c76a460b868c97c9978d1250a88b16dcb284e7ab
sarfaraz786 <md.sarfraz_becsea@avit.ac.in>
2024-05-15 14:23:06 +0530

    update

--- Internship/script.js
+++ Internship/script.js
@@ -1,10 +1,42 @@
-window.onscroll = function() {scrollFunction()};
+const rabBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const rabBtnIcon = menuBtn.querySelector("i");
 
-function scrollFunction() {
-    
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("navbar").style.backgroundColor = "#555";
-  } else {
-    document.getElementById("navbar").style.backgroundColor = "#333";
-  }
-}
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  rabBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  rabBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".content h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".content h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".content h2, .content h6, .content form", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".content .socials span", {
+  ...scrollRevealOption,
+  delay: 2000,
+  interval: 500,
+});
